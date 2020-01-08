f2b6f55ee20328ea1ee720cc7f2b17f3a9016466
GeekyShacklebolt <shivasaxena911@gmail.com>
2020-01-09 02:56:45 +0530

    fix(packages/setup_python): Add shebang + remove packages
    * This remove those python packages that are not needed for
    system wide installation. These packages can always be installed
    in virtual environments of projects as per their requirements.

--- packages/setup_python.sh
+++ packages/setup_python.sh
@@ -1,19 +1,7 @@
+#!/bin/bash
+
 pip install virtualenv
 pip install virtualenvwrapper
 pip install pipenv
-pip install django
-pip install ipython
-pip install ipdb
-pip install dnsyo
-pip install xerox
-pip install fabric
-pip install cookiecutter
-pip install virtualenv
-pip install ghp-import
-pip install piprot
-pip install html2text
-pip install requests[security]
-pip install ansible==2.3.2.0
-pip install storm ssh
-pip install howdoi
+pip install diceware
 
