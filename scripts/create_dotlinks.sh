8720358e36bb0e716f29a89e7c7304136f1a5fbb
GeekyShacklebolt <shivasaxena911@gmail.com>
2020-02-22 15:56:31 +0530

    feat(scripts): Add installation scripts

---
+++ scripts/create_dotlinks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ln ./vim/my_configs.vim ~/.vim_runtime/
+ln ./shell/.shell_aliases ~/
+ln ./git/.git_aliases ~/
+ln ./git/.gitconfig ~/
+ln ./git/.gitignore_global ~/
+ln ./extra/.geekyshacklebolt_shell_title_theme ~/
+ln ./django/.django_aliases ~/
+ln ./zsh/.zshrc ~/
+ln ./zsh/.zshrc_extended ~/
+ln ./bash/.bashrc_extended ~/
+
