7ca02066ba3b6a99a71cc3394f392a06dfefa467
GeekyShacklebolt <shivasaxena911@gmail.com>
2020-07-27 02:26:05 +0530

    fix(travis-CI): Fix linting tests by ignoring submodules
    * Remove explicit shellcheck installation in .travis.yml. Because,
    Travis CI has now integrated ShellCheck by default.

--- tests/test_shell_linting.sh
+++ tests/test_shell_linting.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -e "\e[93mChecking Linting Errors...\033[0m"
-if ! find . -path ./debug -prune -o -type f -name "*.sh" -print0 | xargs -0 -L 1 -n 1 shellcheck;
+if ! find . -path ./forked -prune -o -type f -name "*.sh" -print0 | xargs -0 -L 1 -n 1 shellcheck;
 then
     echo -e "\e[91m\nLinting Failed! Errors are shown above!\033[0m";
 else
